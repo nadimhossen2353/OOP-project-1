a1877a57c0f6ecfdbd96048b0f22bdd7072b1eb4
Nadim Hossen <nadim209255@gmail.com>
2026-04-18 01:03:33 +0600

    Add abstract Vehicle class with properties and methods

---
+++ Vehicle.java
@@ -0,0 +1,49 @@
+abstract class Vehicle {
+    private int vehicleId;
+    private String brand;
+    private String model;
+    private int year;
+    private double rentPerDay;
+    private boolean available = true;
+
+    Vehicle(int vehicleId, String brand, String model,
+            int year, double rentPerDay) {
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.rentPerDay = rentPerDay;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getRentPerDay() {
+        return rentPerDay;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean status) {
+        available = status;
+    }
+
+    void showDetails() {
+        System.out.println("Vehicle ID   : " + vehicleId);
+        System.out.println("Brand        : " + brand);
+        System.out.println("Model        : " + model);
+        System.out.println("Year         : " + year);
+        System.out.println("Rent Per Day : " + rentPerDay + " Tk");
+    }
+
+    double calculateRent(int days) {
+        return rentPerDay * days;
+    }
+}
